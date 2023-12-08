9dca18641e7d7c0a7cb668a24072532e31cd53f4
fuaddx <mi848xfcl@code.edu.az>
2023-12-08 13:34:29 +0400

    BlaBla

---
+++ Contexts/PustokDbContent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pustok2.Models;
+namespace Pustok2.Contexts
+{
+    public class PustokDbContent : DbContext
+    {
+        public PustokDbContent(DbContextOptions opt) : base(opt) { }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImages> ProductImages{get;set;}
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Author>Author { get; set; }
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok2.Areas.Admin.Controllers;
+using Pustok2.Contexts;
+using Pustok2.ModelClass;
+using Pustok2.Models;
+using System.Diagnostics;
+
+namespace Pustok2.Controllers
+{
+    public class HomeController : Controller
+    {
+        PustokDbContent _context { get; }
+
+        public HomeController(PustokDbContent context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var sliders = await _context.Sliders.ToListAsync();
+            var products = await _context.Products.ToListAsync();
+            ProductSliderVM md = new ProductSliderVM();
+            md.Sliders = sliders;
+            md.Products = products;
+            return View(md);
+        }
+
+    }
+}
